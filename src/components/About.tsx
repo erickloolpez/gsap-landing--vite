650efb0090f79648134fa134f9c0d028db6ecf6e
erickloolpez <erickloopeze@hotmail.com>
2025-08-01 15:59:27 -0500

    feat(*): Adding the About us section to our Landing page.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,30 @@
-const About = () => {
+import {SplitText} from  "gsap/all";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+const  About = () => {
+   useGSAP(()=>{
+    const titleSplit = SplitText.create("h2",{
+      type: "words"
+    });
+
+    const scrollTime = gsap.timeline({
+      scrollTrigger:{
+        trigger: "#about",
+        start: "top center",
+      }
+    });
+
+    scrollTime
+      .from(titleSplit.words,{
+        opacity: 0, duration: 1, yPercent: 100, ease: "expo.out", stagger: 0.02
+      })
+      .from(".top-grid div, .bottom-grid div", {
+        opacity: 0, duration: 1, ease: "power1.inOut", stagger: 0.04
+      }, "-=0.5" );
+
+  },[]);
+
   return (
   <div id="about">
       <div className="mb-15 md:px-0 px-5">
